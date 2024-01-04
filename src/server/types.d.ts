22b563d8a4859885f9e9e75ba61ea6c3d84d6623
ItzYanick <me@yanick.gay>
2024-01-04 18:43:09 +0100

    db changes

--- src/server/types.d.ts
+++ src/server/types.d.ts
@@ -2,12 +2,14 @@ declare type User = {
   id: number
   username: string
   password: string
+  isAdmin: boolean
 }
 
 declare type Tunnel = {
   id: number
   owner: number
   name: string
+  description: string
   type: string
   port: number
   hostname: string
